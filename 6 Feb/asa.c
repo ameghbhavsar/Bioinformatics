f64cab8fc130b5fbfc13b7110248be76399aa06b
Amegh Bhsvsar <ameghbhavsar97@gmail.com>
2018-02-06 11:25:56 +0530

    Add asa.c

---
+++ 6 Feb/asa.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+#include<math.h>
+
+void main(){
+  FILE *comp,*A,*B,*C,*D,*E,*F,*G,*H;
+  comp=fopen("5un8.asa","r");
+  
+}
